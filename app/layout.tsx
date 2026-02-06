492c8e9c8afa03c64dd1c22662740ce38cba6605
Max R <mxr@users.noreply.github.com>
2026-02-06 09:57:01 -0500

    Add footer version and license (#8)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Space_Grotesk } from "next/font/google";
+import packageJson from "../package.json";
 import "./globals.css";
 
 const spaceGrotesk = Space_Grotesk({
@@ -29,6 +30,12 @@ export default function RootLayout({
         className={`${spaceGrotesk.variable} ${plexMono.variable} antialiased`}
       >
         {children}
+        <footer className="border-t border-white/60 bg-white/40 px-6 py-4 text-xs text-[#5b5148] sm:px-10">
+          <div className="mx-auto flex max-w-6xl flex-col gap-1 sm:flex-row sm:items-center sm:justify-between">
+            <span>Version {packageJson.version}</span>
+            <span>Licensed under GNU Affero General Public License v3.0</span>
+          </div>
+        </footer>
       </body>
     </html>
   );
